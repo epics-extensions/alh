062736c7a8264adfa750c93b64eb6e0c854190cd
Ken Evans <evans@aps.anl.gov>
1998-06-04 12:31:58 -0500

    Changed system("command &") to _spawnl("command") for WIN32 in processSpawn_callback.  Added test.win32.alhConfig.

--- process.c
+++ process.c
@@ -1,5 +1,9 @@
 /*
  $Log$
+ Revision 1.5  1998/06/04 17:31:58  evans
+ Changed system("command &") to _spawnl("command") for WIN32 in
+ processSpawn_callback.  Added test.win32.alhConfig.
+
  Revision 1.4  1995/10/20 16:50:50  jba
  Modified Action menus and Action windows
  Renamed ALARMCOMMAND to SEVRCOMMAND
@@ -57,6 +61,10 @@ static char *sccsId = "@(#)process.c	1.4\t8/12/93";
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#ifdef WIN32
+#include <process.h>     /* For spawn */
+#endif
 #include <Xm/Xm.h>
 #include <alLib.h>
 #include <ax.h>
@@ -90,19 +98,48 @@ Widget w;
 char *command;
 void * call_data;
 {
-char buff[120];
-int l;
+    char buff[120];
+    int l;
+    int status;
+#ifdef WIN32
+    static int first=1;
+    static char *ComSpec;
+#endif    
 
-
-
-	if (command) {
+    if (command) {
+      /* Strip any LF from the end */
 	l = strlen(command);
 	if (*(command+l-1) == '\n') *(command+l-1) = ' ';
-	    	sprintf(buff,"%s &",command);
-	    	system(buff);
-		}
-
-
+#ifdef WIN32
+	sprintf(buff,"%s",command);
+      /* Get ComSpec for the command shell (should be defined) */
+	if (first) {
+	    first=0;
+	    ComSpec = getenv("ComSpec");
+	}
+	if (!ComSpec) {
+	    errMsg("processSpawn_callback: Cannot find command processor\n");
+	    return;
+	}
+	status = _spawnl(_P_DETACH, ComSpec, ComSpec, "/C", buff, NULL);
+#else
+	sprintf(buff,"%s &",command);
+	status=system(buff);
+#endif	
+	if(status == -1) {
+	  /* System call failed */
+	    char *errstring=strerror(errno);
+	    
+	    errMsg("processSpawn_callback: Cannot process command:\n"
+	      "%s\n  %s",
+	      buff,errstring);
+	} else if (status > 0) {
+	  /* Assume program returned an error */
+	    errMsg("processSpawn_callback: Command returned %d:\n"
+	      "%s\n",
+	      status,buff);
+	}
+    }
 }	
 
 /***************************************************
