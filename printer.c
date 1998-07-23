624470e109698591edd1e01703d073e03b7567e6
Janet B. Anderson <jba@aps.anl.gov>
1998-07-23 11:11:21 -0500

    Added some standard library inclues.

--- printer.c
+++ printer.c
@@ -1,4 +1,7 @@
 /*Printer routine. Work like independent process. Albert */
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
