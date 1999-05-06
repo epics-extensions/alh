e98a25fa3685c1b46ce378b4915c1a053d9d6458
epics <epics>
1999-05-06 16:22:50 +0000

    Helios build Thu May  6 10:44:21 CDT 1999

--- version.h
+++ version.h
@@ -11,7 +11,7 @@ static char *versionhsccsId = "@(#) $Id$";
 
 #define ALH_VERSION       1
 #define ALH_REVISION      1
-#define ALH_MODIFICATION  2
+#define ALH_MODIFICATION  3
 
 #define ALH_CREDITS_STRING  \
           "Developed at Argonne National Laboratory\n\n" \
