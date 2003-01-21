03073e38d2dc5823721a0db679514bfb14f1d5e9
Janet B. Anderson <jba@aps.anl.gov>
2003-01-21 16:06:03 -0600

    Added noAckWindow.

--- axArea.h
+++ axArea.h
@@ -83,6 +83,7 @@ typedef struct  areaLink{
 	void             *forcePVWindow;
 	void             *maskWindow;
 	void             *beepSevrWindow;
+	void             *noAckWindow;
 	/* ----- Current Selection groupLine info ----- */
 	void             *selectionWindow;
 	void             *selectionLink;
