6d102b9fc3129d4dd4150bc7e1cfb480863a9b60
Janet B. Anderson <jba@aps.anl.gov>
2012-09-11 13:11:26 -0500

    ALH_1_2_30

--- version.h
+++ version.h
@@ -23,9 +23,9 @@
 
 #define ALH_VERSION       1
 #define ALH_REVISION      2
-#define ALH_MODIFICATION 29
+#define ALH_MODIFICATION 30
 
-#define ALH_VERSION_STRING "ALH Version 1.2.29"
+#define ALH_VERSION_STRING "ALH Version 1.2.30"
 
 #define ALH_CREDITS_STRING  \
           "Developed at Argonne National Laboratory\n\n" \
